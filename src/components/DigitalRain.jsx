3336913f9d99394750df699e4b0a942660950ac3
fcvirtual <austin@fcvt.dev>
2023-07-14 15:29:46 -0400

    bug fix

--- src/components/DigitalRain.jsx
+++ src/components/DigitalRain.jsx
@@ -10,20 +10,22 @@ const DigitalRain = () => {
         let width = (canvas.width = window.innerWidth);
         let height = (canvas.height = window.innerHeight);
         let columns = Math.floor(width / 20)
-        const characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン'
+        const characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         const charArray = characters.split("")
         let drops = [];
         for (let i = 0; i < columns; i++) {
             drops[i] = 1;
+        }
 
             // optimal speed between 20-30
             let frameRate = 25;
             let lastFrameTime = Date.now()
 
             const draw = () => {
-                ctx.fillStyle = "rgba(0, 0, 0, 0.04)"
-                ctx.fillRect(0, 0, width, height)
+                ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'
+                ctx.textAlign = 'center';
 
+                ctx.fillRect(0, 0, width, height)
                 ctx.fillStyle = "#0f0"
 
                 ctx.font = "15px monospace"
@@ -31,7 +33,7 @@ const DigitalRain = () => {
                     const text = charArray[Math.floor(Math.random() * charArray.length)]
                     ctx.fillText(text, i * 20, drops[i] * 20)
 
-                    if (drops[i] * 20 > height && Math.random() > 0.975) {
+                    if (drops[i] * 20 > height && Math.random() > 0.98) {
                         drops[i] = 0
                     }
 
@@ -74,7 +76,7 @@ const DigitalRain = () => {
                 }
             }
 
-        }
+        
     })
     return <canvas className="fixed top-0 left-0 z-[-1]" ref={canvasRef}></canvas>;
 }
